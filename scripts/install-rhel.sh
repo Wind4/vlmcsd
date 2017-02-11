211e463ddd71f5df884080ce98d0fc5eb369b51f
Wind4 <puxiaping@gmail.com>
2017-02-11 17:43:31 +0800

    Update to svn1108

--- scripts/install-rhel.sh
+++ scripts/install-rhel.sh
@@ -41,7 +41,7 @@ TMP_DIR=`mktemp -d`
 cd ${TMP_DIR}
 
 echo 'Downloading vlmcsd ...'
-wget -q https://github.com/Wind4/vlmcsd/releases/download/svn1107/binaries.tar.gz -O binaries.tar.gz
+wget -q https://github.com/Wind4/vlmcsd/releases/download/svn1108/binaries.tar.gz -O binaries.tar.gz
 check_result $? 'Download vlmcsd failed.'
 
 echo 'Downloading startup script ...'
